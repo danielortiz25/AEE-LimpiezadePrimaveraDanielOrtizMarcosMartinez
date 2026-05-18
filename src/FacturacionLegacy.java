080e7c7086006f39ce208125818f7992bb73f210
marcosmartinez25-debug <marcosmartinezcossio@gmail.com>
2026-05-18 13:37:54 +0200

    cambio tC por tipoCliente

--- src/FacturacionLegacy.java
+++ src/FacturacionLegacy.java
@@ -5,15 +5,15 @@
 public class FacturacionLegacy {
 
     // Método a refactorizar
-    public double calcularTotal(double importeBase, int tC, boolean dV) {
+    public double calcularTotal(double importeBase, int tipoCliente, boolean dV) {
         if (importeBase > 0) {
-            if (tC == 1) {
+            if (tipoCliente == 1) {
                 if (dV == true)
                     return importeBase - (importeBase * 0.25);
                 else
                     return importeBase - (importeBase * 0.15);
             } else {
-                if (tC == 2) {
+                if (tipoCliente == 2) {
                     return importeBase - (importeBase * 0.05);
                 } else {
                     return importeBase;
