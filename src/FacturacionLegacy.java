72aeb3d97aac4ac1f7ec82bc9855ef3af9f57964
marcosmartinez25-debug <marcosmartinezcossio@gmail.com>
2026-05-18 13:44:03 +0200

    Cambios en FacturacionLegacy.java - Agrego una constante para el IVA

--- src/FacturacionLegacy.java
+++ src/FacturacionLegacy.java
@@ -4,17 +4,21 @@
  */
 public class FacturacionLegacy {
 
+    private static final double DESCUENTO_VIP_EXTRA = 0.25;
+    private static final double DESCUENTO_VIP = 0.15;
+    private static final double DESCUENTO_ESTANDAR = 0.05;
+
     // Método a refactorizar
     public double calcularTotal(double importeBase, int tipoCliente, boolean esSocioVip) {
         if (importeBase > 0) {
             if (tipoCliente == 1) {
                 if (esSocioVip == true)
-                    return importeBase - (importeBase * 0.25);
+                    return importeBase - (importeBase * DESCUENTO_VIP_EXTRA);
                 else
-                    return importeBase - (importeBase * 0.15);
+                    return importeBase - (importeBase * DESCUENTO_VIP);
             } else {
                 if (tipoCliente == 2) {
-                    return importeBase - (importeBase * 0.05);
+                    return importeBase - (importeBase * DESCUENTO_ESTANDAR);
                 } else {
                     return importeBase;
                 }
